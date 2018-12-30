5840116a790c0bfbda6ad9018fd5ffa53c6952be
Stefan Hauschildt <stefan.h@uschildt.de>
2018-12-30 19:58:00 +0100

    - add Size PREVIEW

--- src/main/kotlin/fitness/freya/newsservice/model/Size.kt
+++ src/main/kotlin/fitness/freya/newsservice/model/Size.kt
@@ -2,10 +2,8 @@ package fitness.freya.newsservice.model
 
 enum class Size(val preferedSize: Int) {
   ORIGINAL(0),
-  MINI(48),
+  PREVIEW(250),
   XS(600),
   SM(960),
-  MD(1280),
-  LG(1920),
-  XL(2569)
+  MD(1280)
 }
