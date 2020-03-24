13b9472c8ca4cf4116d206f716de44c7bd3afd3f
Stefan Hauschildt <stefan.h@uschildt.de>
2020-03-24 13:52:45 +0100

    remove CrossOrigin Annotation for local testing

--- src/main/kotlin/fitness/freya/newsservice/api/NewsController.kt
+++ src/main/kotlin/fitness/freya/newsservice/api/NewsController.kt
@@ -4,7 +4,6 @@ import fitness.freya.newsservice.mapping.NewsMapper
 import fitness.freya.newsservice.service.NewsService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-@CrossOrigin(origins = ["localhost:3333", "www.freya.fitness"], allowedHeaders = ["*"])
 @RestController
 @RequestMapping("/news")
 class NewsController(
